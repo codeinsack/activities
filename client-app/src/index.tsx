499c1a3dee828b2af7fd3c00b57d0c8ad4f31f54
Eugen Furmanov <eugenfurmanov@coherentsolutions.com>
2023-05-26 21:53:35 +0200

    add NavBar component

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(<App />);
-
-reportWebVitals();
