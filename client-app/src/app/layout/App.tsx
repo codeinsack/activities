06338448398c865cbd2aa8a7ca97ee38f2d5562b
codeinsack <codeinsack@gmail.com>
2023-05-28 15:20:56 +0200

    add mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,5 @@
 import { v4 as uuid } from "uuid";
+import { observer } from "mobx-react-lite";
 import { Container } from "semantic-ui-react";
 import React, { useEffect, useState } from "react";
 
@@ -7,8 +8,11 @@ import agent from "../api/agent";
 import { Activity } from "../models/activity";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import LoadingComponent from "./LoadingComponent";
+import { useStore } from "../stores/store";
 
 function App() {
+  const { activityStore } = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
@@ -82,6 +86,8 @@ function App() {
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: "7em" }}>
+        <h2>{activityStore.title}</h2>
+        <button onClick={activityStore.setTitle}>Click me</button>
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
@@ -99,4 +105,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,17 @@
+import "semantic-ui-css/semantic.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "semantic-ui-css/semantic.min.css";
+
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
+import { store, StoreContext } from "./app/stores/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+
+root.render(
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>
+);
