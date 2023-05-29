5ab9366591192c043d27f4dace255bbd3ee72b47
codeinsack <codeinsack@gmail.com>
2023-05-29 22:50:31 +0200

    handle http errors

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
 using API.Extensions;
+using Application.Activities;
+using FluentValidation.AspNetCore;
 
 namespace API
 {
@@ -14,7 +16,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config =>
+            {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
         }
 
