c8b7c74e6dcdd9f06f9181699948c9facf41b48b
codeinsack <codeinsack@gmail.com>
2023-06-09 23:31:19 +0200

    send and receive comments

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,7 +18,10 @@ public static class ApplicationServiceExtensions
         services.AddCors(opt =>
         {
             opt.AddPolicy("CorsPolicy",
-                policy => { policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"); });
+                policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:3000");
+                });
         });
         services.AddMediatR(typeof(List.Handler).Assembly);
         services.AddAutoMapper(typeof(MappingProfiles).Assembly);
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -3,12 +3,14 @@ import { createContext, useContext } from "react";
 import CommonStore from "./commonStore";
 import UserStore from "./userStore";
 import ModalStore from "./modalStore";
+import CommentStore from "./commentStore";
 
 interface Store {
   activityStore: ActivityStore;
   commonStore: CommonStore;
   userStore: UserStore;
   modalStore: ModalStore;
+  commentStore: CommentStore;
 }
 
 export const store: Store = {
@@ -16,6 +18,7 @@ export const store: Store = {
   commonStore: new CommonStore(),
   userStore: new UserStore(),
   modalStore: new ModalStore(),
+  commentStore: new CommentStore(),
 };
 
 export const StoreContext = createContext(store);
