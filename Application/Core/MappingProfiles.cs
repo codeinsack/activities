f8c92e2b15ef12b7e3deb826bf8881572a025302
codeinsack <codeinsack@gmail.com>
2023-06-12 17:56:49 +0200

    finish with followers logic

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -20,5 +20,8 @@ public class MappingProfiles : Profile
         CreateMap<Comment, CommentDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
             .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName));
+        CreateMap<AppUser, Profiles.Profile>()
+            .ForMember(d => d.FollowersCount, o => o.MapFrom(s => s.Followers.Count))
+            .ForMember(d => d.FollowingCount, o => o.MapFrom(s => s.Followings.Count));
     }
 }
--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -5,4 +5,7 @@ public class Profile
     public string Username { get; set; }
     public string DisplayName { get; set; }
     public string Bio { get; set; } = "Default Bio";
+    public bool Following { get; set; }
+    public int FollowersCount { get; set; }
+    public int FollowingCount { get; set; }
 }
